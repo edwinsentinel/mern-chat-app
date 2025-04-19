744282d0207cd174f0c06370a5df665f4b1062cf
edwinsentinel <waweruedwin2@gmail.com>
2025-04-19 13:32:53 +0400

    created a signup controller and hashed the passowrds

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,49 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
+/// @desc    Register a new user
 export const signup = async (req, res) => {
  try {
     const {fullName,username,password,confirmPassword,gender} =req.body;
-    
+
+    if(password !== confirmPassword){
+        return res.status(400).json({message:"Passwords do not match"});
+    }
+    const user= await User.findOne({username});
+    if(user){
+        return res.status(400).json({message:"User already exists"});
+    }
+    //HASH PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //https://avatar-placeholder.iran.liara.run/avatar/200/200/any
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boyId?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girlId?username=${username}`
+
+
+    const newUser = new User({
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic
+    });
+
+
  } catch (error) {
-    
+    console.error("Error signup controller:", error.message);
+    res.status(500).json({message:"Internal server error"});
  }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDb from './db/connectToMongoDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-//routes
-app.get('/', (req, res) => {
-  res.send('Hello World!&*');
-});
 
+dotenv.config();
+
+app.use(express.json());//middleware to parse JSON data
 
 //middleware
 app.use("/api/auth",authRoutes);
+
+//routes
+//app.get('/', (req, res) => {
+//  res.send('Hello World!&*');
+//});
+
+
+
 
 
 //port
