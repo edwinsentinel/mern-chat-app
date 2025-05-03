d66e8acf70e0b5a3637d53072600e27a472424ba
edwinsentinel <waweruedwin2@gmail.com>
2025-05-03 17:14:05 +0400

    created signup and sidebar

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import MessageContainer from '../../components/messageContainer/MessageContainer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'></div>
+    <Sidebar />
+    <MessageContainer />
+    
   )
 }
 
-export default Home
+export default Home;
